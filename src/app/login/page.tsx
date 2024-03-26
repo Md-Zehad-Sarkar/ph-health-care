dd91f12159cd038e0ce8a64130888d6efc42d003
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-03-26 22:56:19 +0600

    register and login page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,83 @@
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
+import Link from "next/link";
+
 const LoginPage = () => {
   return (
-    <div>
-      <h2>login page</h2>
-    </div>
+    <Container>
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          textAlign: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            p: 4,
+            boxShadow: 1,
+            borderRadius: "10px",
+          }}
+        >
+          <Stack
+            sx={{ justifyContent: "center", alignItems: "center", gap: 1 }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} alt="logo" />
+            </Box>
+            <Box>
+              <Typography>Login PH Health Care</Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <form>
+              <Grid container spacing={2} sx={{my:2}}>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="text"
+                    label="Email"
+                    variant="outlined"
+                    fullWidth={true}
+                    size="small"
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="text"
+                    label="Password"
+                    variant="outlined"
+                    fullWidth={true}
+                    size="small"
+                  />
+                </Grid>
+              </Grid>
+              <Typography textAlign="end">
+                <Link href="/forgot-password">Forgot password?</Link>
+              </Typography>
+              <Button sx={{ margin: "10px 0px", width: "100%" }}>Login</Button>
+              <Typography>
+                Don&apos;t have an account?{" "}
+                <Link href="/register">Register</Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,107 @@
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
+import Link from "next/link";
+
 const RegisterPage = () => {
   return (
-    <div>
-      <h2>register page</h2>
-    </div>
+    <Container>
+      <Stack sx={{ justifyContent: "center", alignItems: "center",height:'100vh',textAlign:'center' }}>
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            p: 4,
+            boxShadow: 1,
+            borderRadius: "10px",
+          }}
+        >
+          <Stack
+            sx={{ justifyContent: "center", alignItems: "center", gap: 2 }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} alt="logo" width={50} height={50} />
+            </Box>
+            <Box>
+              <Typography>Patient Register</Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <form>
+              <Grid container spacing={2} my={1}>
+                <Grid item md={12}>
+                  <TextField
+                    id="outlined-basic"
+                    type="text"
+                    label="Name"
+                    variant="outlined"
+                    fullWidth={true}
+                    size="small"
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="text"
+                    label="Email"
+                    variant="outlined"
+                    fullWidth={true}
+                    size="small"
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="password"
+                    label="Password"
+                    variant="outlined"
+                    fullWidth={true}
+                    size="small"
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="tel"
+                    label="Contact no"
+                    variant="outlined"
+                    fullWidth={true}
+                    size="small"
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="text"
+                    label="Address"
+                    variant="outlined"
+                    fullWidth={true}
+                    size="small"
+                  />
+                </Grid>
+              </Grid>
+              <Button sx={{ margin: "10px 0px", width: "100%" }}>
+                Register
+              </Button>
+              <Box>
+                <Typography>
+                  Do you already have an account?{" "}
+                  <Link href="/login">Login</Link>
+                </Typography>
+              </Box>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
