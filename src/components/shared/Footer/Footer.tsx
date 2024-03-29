ec21f6be04b3cc30088badb90e796ec2030010be
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-03-29 17:54:39 +0600

    code changes for styles

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -9,43 +9,59 @@ import linkedinIcon from "@/assets/landing_page/linkedin.png";
 const Footer = () => {
   return (
     <Box bgcolor="rgb(17,26,34)">
-      <Stack direction="row" justifyContent="center" gap={2} py={2}>
-        <Typography color="white" component={Link} href="/consultation">
-          Consultation
-        </Typography>
-        <Typography color="white" component={Link} href="/health-plans">
-          Health Plans
-        </Typography>
-        <Typography color="white" component={Link} href="/medicine">
-          Medicine
-        </Typography>
-        <Typography color="white" component={Link} href="/diagnostics">
-          Diagnostics
-        </Typography>
-        <Typography color="white" component={Link} href="/ngos">
-          NGOs
-        </Typography>
-      </Stack>
-      <Stack direction="row" gap={4} py={3} justifyContent="center">
-        <Image src={facebookIcon} alt="fb" width={30} height={30} />
-        <Image src={instagramIcon} alt="fb" width={30} height={30} />
-        <Image src={twitterIcon} alt="fb" width={30} height={30} />
-        <Image src={linkedinIcon} alt="fb" width={30} height={30} />
-      </Stack>
-      <div className="border-b-[1px] border-dashed py-4"></div>
-      <Stack direction="row" justifyContent="space-between" py={3}>
-        <Typography component="p" color="white">
-          &copy;2024 PH-HEALTH-CARE. All rights reserved.
-        </Typography>
-        <Typography variant="h5" component="h1" fontWeight={600} color="white">
-          P
-          <Box component="span" color="primary.main">
-            H
-          </Box>
-          Health Care
-        </Typography>
-        <Typography color="white">Privacy policy! Terms&Conditions</Typography>
-      </Stack>
+      <Box sx={{ maxWidth: "1200px", margin: "auto" }}>
+        <Stack direction="row" justifyContent="center" gap={2} py={2}>
+          <Typography color="white" component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography color="white" component={Link} href="/health-plans">
+            Health Plans
+          </Typography>
+          <Typography color="white" component={Link} href="/medicine">
+            Medicine
+          </Typography>
+          <Typography color="white" component={Link} href="/diagnostics">
+            Diagnostics
+          </Typography>
+          <Typography color="white" component={Link} href="/ngos">
+            NGOs
+          </Typography>
+        </Stack>
+        <Stack direction="row" gap={4} py={3} justifyContent="center">
+          <Image src={facebookIcon} alt="fb" width={30} height={30} />
+          <Image src={instagramIcon} alt="fb" width={30} height={30} />
+          <Image src={twitterIcon} alt="fb" width={30} height={30} />
+          <Image src={linkedinIcon} alt="fb" width={30} height={30} />
+        </Stack>
+
+        <Box
+          sx={{
+            border: "1px dashed lightgray",
+            maxWidth: "1200px",
+            margin: "auto",
+          }}
+        ></Box>
+        <Stack direction="row" justifyContent="space-between" py={3}>
+          <Typography component="p" color="white">
+            &copy;2024 PH-HEALTH-CARE. All rights reserved.
+          </Typography>
+          <Typography
+            variant="h5"
+            component="h1"
+            fontWeight={600}
+            color="white"
+          >
+            P
+            <Box component="span" color="primary.main">
+              H
+            </Box>
+            Health Care
+          </Typography>
+          <Typography color="white">
+            Privacy policy! Terms&Conditions
+          </Typography>
+        </Stack>
+      </Box>
     </Box>
   );
 };
