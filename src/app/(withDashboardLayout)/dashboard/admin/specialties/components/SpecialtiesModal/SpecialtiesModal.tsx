83f33c79f01cd2f16a19ac3b2da117ac587078a4
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-04-02 06:09:25 +0600

    reusable file uploader success

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialtiesModal/SpecialtiesModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialtiesModal/SpecialtiesModal.tsx
@@ -1,15 +1,32 @@
 import PHModal from "@/components/shared/PHModal/PHModal";
-import { TextField } from "@mui/material";
+import PHFileUploader from "@/forms/PHFileUploader";
+import PHForm from "@/forms/PHForm";
+import PHInput from "@/forms/PHInput";
+import { Button, Grid, TextField } from "@mui/material";
 import { Dispatch } from "react";
+import { FieldValues } from "react-hook-form";
 type TSpecialtiesModalProps = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const SpecialtiesModal = ({ open, setOpen }: TSpecialtiesModalProps) => {
+  const handleCreateSpecialty = (values: FieldValues) => {
+    console.log(values);
+  };
   return (
     <PHModal open={open} setOpen={setOpen} title="Create Specialty">
-      <TextField />
+      <PHForm onSubmit={handleCreateSpecialty}>
+        <Grid container spacing={2}>
+          <Grid item md={6}>
+            <PHInput name="title" label="Title" type="text" fullWidth={false} />
+          </Grid>
+          <Grid item md={6}>
+            <PHFileUploader name="file" label="File Upload" />
+          </Grid>
+        </Grid>
+        <Button type="submit" sx={{ mt: 1 }}>Create</Button>
+      </PHForm>
     </PHModal>
   );
 };
